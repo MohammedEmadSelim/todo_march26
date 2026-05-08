cfcfa7558d755011df2c61b574b69c55902a6222
Mohammed <mohammedemad557@yahoo.com>
2026-05-08 13:54:17 +0300

    correct

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
+import 'package:todo_march26/features/splash/presentation/ui_screens/splash_sceen.dart';
 
 // we following clean archeticeture pattern
 // separation of concerns
